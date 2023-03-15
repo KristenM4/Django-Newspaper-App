ea79652bceebca87cd83942d6af1f8c36b5a31fc
Kristen Martin <kristen4810@gmail.com>
2023-03-15 20:48:20 +0800

    create and register ArticleAdmin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "date",
+        "body"
+    ]
+    list_filter = ("author", "date",)
+
+admin.site.register(Article, ArticleAdmin)
