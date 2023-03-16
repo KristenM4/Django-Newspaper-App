7b7f2749870be3ccd88ba737c7069b84f9d12a30
Kristen Martin <kristen4810@gmail.com>
2023-03-17 04:40:15 +0800

    add debug to env var, add allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
